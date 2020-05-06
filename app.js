52531fe073181aaa53af1c40dd2d03e1b2164b0d
Doggo <dhernandez24e@hotmail.com>
2020-05-05 17:54:54 -0700

    inquirer implemented

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const portfolioData = process.argv.slice(2, process.argv.length);
+// const [name, github] = portfolioData;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(portfolioData);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
