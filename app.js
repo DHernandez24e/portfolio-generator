643e24999c767ce9084f8b437f4ebcc8871dc976
Doggo <dhernandez24e@hotmail.com>
2020-05-06 21:17:27 -0700

    added array methods to distribute data to HTML properly

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 // const portfolioData = process.argv.slice(2, process.argv.length);
 // const [name, github] = portfolioData;
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-// const pageHTML = generatePage(portfolioData);
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -138,5 +130,12 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!')
+        });
         console.log(portfolioData);
     });
